b18ea8c4a7bbe575af46f21401deef7656024bb4
Leelsmuth <femianjorin@rocketmail.com>
2019-10-14 21:07:18 +0100

    refactored code

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -12,25 +12,9 @@ beforeEach(() => {
 
 describe("Dashboard", () => {
   it("shows the controls and display", () => {
-    expect(wrapper.queryByText(/close Gate/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
     expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
     expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
     expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
-  });
-  it("cannot be closed or opened if it is locked", () => {
-    rtl.fireEvent.click(wrapper.getByTestId("toggleClosed"));
-    rtl.fireEvent.click(wrapper.getByTestId("toggleLocked"));
-    wrapper.debug();
-    expect(wrapper.getByTestId("toggleLocked")).toBeEnabled();
-    expect(wrapper.getByTestId("toggleClosed")).toBeDisabled();
-  });
-
-  it("buttons change text on click", () => {
-    rtl.fireEvent.click(wrapper.getByTestId("toggleClosed"));
-    expect(wrapper.getByTestId("toggleClosed")).toHaveTextContent(/open gate/i);
-    rtl.fireEvent.click(wrapper.getByTestId("toggleLocked"));
-    expect(wrapper.getByTestId("toggleLocked")).toHaveTextContent(
-      /unlock gate/i
-    );
   });
 });
