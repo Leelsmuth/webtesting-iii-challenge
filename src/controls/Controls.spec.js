d2410a3469f4bfdd07d2e3eb120c8efb47b2294b
Leelsmuth <femianjorin@rocketmail.com>
2019-10-14 17:30:04 +0100

    MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,36 @@
 // Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Controls />);
+});
+
+describe("Controls renders without crashing", () => {
+  test("Controls provides buttons to toggle the closed and locked states", () => {
+    expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+  });
+  //   test("buttons' text changes to reflect the state the door will be in if clicked", () => {
+  //     expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+  //     rtl.fireEvent.click(wrapper.queryByText(/close gate/i));
+  //     expect(wrapper.queryByText(/close gate/i)).not.toBeDisplay();
+  //     expect(wrapper.queryByText(/open gate/i)).toBeDisplay();
+  //   });
+
+  it("the closed toggle button is disabled if the gate is locked", () => {
+    rtl.cleanup();
+    wrapper = rtl.render(<Controls locked={true} closed={true} />);
+    expect(wrapper.getByTestId("toggleClosed")).toBeDisabled();
+  });
+
+  it("the locked toggle button is disabled if the gate is open", () => {
+    expect(wrapper.getByTestId("toggleLocked")).toBeDisabled();
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,36 @@
-// Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+let wrapper;
+
+afterEach(rtl.cleanup);
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />);
+});
+
+describe("Dashboard", () => {
+  it("shows the controls and display", () => {
+    expect(wrapper.queryByText(/close Gate/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+  });
+  it("cannot be closed or opened if it is locked", () => {
+    rtl.fireEvent.click(wrapper.getByTestId("toggleClosed"));
+    rtl.fireEvent.click(wrapper.getByTestId("toggleLocked"));
+    wrapper.debug();
+    expect(wrapper.getByTestId("toggleLocked")).toBeEnabled();
+    expect(wrapper.getByTestId("toggleClosed")).toBeDisabled();
+  });
+
+  it("buttons change text on click", () => {
+    rtl.fireEvent.click(wrapper.getByTestId("toggleClosed"));
+    expect(wrapper.getByTestId("toggleClosed")).toHaveTextContent(/open gate/i);
+    rtl.fireEvent.click(wrapper.getByTestId("toggleLocked"));
+    expect(wrapper.getByTestId("toggleLocked")).toHaveTextContent(
+      /unlock gate/i
+    );
+  });
+});
